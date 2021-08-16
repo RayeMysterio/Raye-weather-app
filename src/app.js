d351a360919f1bf7aa0cf5f598ffaf1db9b8d6aa
RayeMysterio <88468157+RayeMysterio@users.noreply.github.com>
2021-08-15 19:45:09 -0500

    Updating HTML/CSS code for Background Image
    I altered the coding for my background image (lightclouds.jpg).

---
+++ src/app.js
@@ -0,0 +1,90 @@
+let dateElement = document.querySelector("#current-date");
+let currentTime = new Date();
+
+let hours = currentTime.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = currentTime.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let day = currentTime.getDay();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+console.log(days[day]);
+console.log(`${hours}`);
+console.log(`${minutes}`);
+
+dateElement.innerHTML = `${days[day]} ${hours}:${minutes}`;
+
+function displayWeatherCondition(response) {
+  console.log();
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temp").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchCity(city) {
+  let apiKey = "def590e2e2448ec680d9dcf0bbd9e3b1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  searchCity(city);
+}
+
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  let temp = (tempElement.innerHTML = 67);
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#temp");
+  let temp = (tempElement.innerHTML = 24);
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+searchCity("New York");
+
+function searchLocation(position) {
+  let apiKey = "def590e2e2448ec680d9dcf0bbd9e3b1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector(".current-location");
+currentLocationButton.addEventListener("click", getCurrentLocation);
