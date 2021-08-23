093123352aa78d0672d7faaa3629f28a55c28bda
RayeMysterio <88468157+RayeMysterio@users.noreply.github.com>
2021-08-22 22:05:41 -0500

    Updated API Integration (JS)
    Updated JS with proper API Integration code via the Weather API.

--- src/app.js
+++ src/app.js
@@ -27,6 +27,58 @@ console.log(`${minutes}`);
 
 dateElement.innerHTML = `${days[day]} ${hours}:${minutes}`;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class ="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+    <div class="col-2">
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <img
+        src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
+        alt=""
+        width="38"
+      />
+      </br>
+      <span class="col-2">
+        <span class="degree-week-max"> ${Math.round(
+          forecastDay.temp.max
+        )}° </span>
+        <span class="degree-week-min"> ${Math.round(
+          forecastDay.temp.min
+        )}° </span>
+    </span>
+  </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "def590e2e2448ec680d9dcf0bbd9e3b1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   document.querySelector("#temp").innerHTML = Math.round(
@@ -38,6 +90,8 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
